e8229cb5a57ba949c3c7fbb461f98372f6cefe62
zchillah <32843881+zchillah@users.noreply.github.com>
2018-11-16 12:40:39 +0100

    Update IA_partie1.py

--- IA_partie1.py
+++ IA_partie1.py
@@ -1,1 +1,9 @@
+"""
+    python 3 
+    Auteur : CHILLAH Zakaria
+    Matricule : 000442038
+    Projet d'année: "Partie 1: Apprentissage par renforcements"
+    INFO-F-106 : Projet d'informatique
+"""
+
 
